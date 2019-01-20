37c175194a6388055298efe6a7010446510b7a38
Shelton <sheltonwlsn@gmail.com>
2019-01-20 02:23:08 +0200

    Navbar initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import './Navbar.css';
+import logo from '../img/navlogocolor.png';
+
+class Navbar extends Component {
+    render(){
+        return (
+            <div className="container">
+                <a href="../index.html"><img className="logo" alt="site logo" src={logo}/></a>
+                <div className="navcontainer">
+                    <ul className="navgroup">
+                        <li><a className="navlink" href="#">Home</a></li>
+                        <li><a className="navlink" href="#">About</a></li>
+                        <li><a className="navlink" href="#">Services</a></li>
+                        <li><a className="navlink" href="#">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
