0bd0513a161cc3ded4e1ee61d63294f8205c7cf0
Shelton <sheltonwlsn@gmail.com>
2019-01-20 02:54:26 +0200

    navbar arrangement and style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,15 +5,17 @@ import logo from '../img/navlogocolor.png';
 class Navbar extends Component {
     render(){
         return (
-            <div className="container">
-                <a href="../index.html"><img className="logo" alt="site logo" src={logo}/></a>
-                <div className="navcontainer">
-                    <ul className="navgroup">
-                        <li><a className="navlink" href="#">Home</a></li>
-                        <li><a className="navlink" href="#">About</a></li>
-                        <li><a className="navlink" href="#">Services</a></li>
-                        <li><a className="navlink" href="#">Contact</a></li>
-                    </ul>
+            <div className="navbar">
+                <div className="container">
+                    <a href="../index.html"><img className="logo" alt="site logo" src={logo}/></a>
+                    <div className="navcontainer">
+                        <ul className="navgroup">
+                            <li><a className="navlink" href="#">Home</a></li>
+                            <li><a className="navlink" href="#">About</a></li>
+                            <li><a className="navlink" href="#">Services</a></li>
+                            <li><a className="navlink" href="#">Contact</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         );
